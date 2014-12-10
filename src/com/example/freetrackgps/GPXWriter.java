f19bf0158a699c8a905e1000967489e612fa60c7
sylwek3100 <sylwek3100@gmail.com>
2014-12-10 14:51:25 +0100

    Add time creation to GPX file

--- src/com/example/freetrackgps/GPXWriter.java
+++ src/com/example/freetrackgps/GPXWriter.java
@@ -3,15 +3,17 @@ package com.example.freetrackgps;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.channels.Pipe;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+import java.util.Calendar;
 
 public class GPXWriter {
 	private String filepath;
 	private StringBuilder content;
 	private Boolean isOpen = false;
 	private BufferedWriter bufferedWriter;
+    private Calendar calendar;
 	private String XMLHEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
 	private static final SimpleDateFormat POINT_DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 	private static final String GPXINIT = "<gpx"
@@ -20,11 +22,13 @@ public class GPXWriter {
              + " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\""
              + " xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd \">";
 	public GPXWriter(String fileName) {
+        calendar = Calendar.getInstance();
         content = new StringBuilder();
 		filepath = fileName;
 		content.append(XMLHEADER + GPXINIT+ "\n");
 		content.append("<metadata>\n<author>GPX Track</author>\n</metadata>");
-		content.append("<trk>\n<name>GPX Workout</name>\n<trkseg>\n");
+		content.append("<trk>\n<name>GPX Workout</name>\n<time>" + POINT_DATE_FORMATTER.format(calendar.getTime()) +"</time>\n");
+        content.append("<trkseg>\n");
 		try {
 			bufferedWriter = new BufferedWriter(new FileWriter(filepath));
 			write(content.toString());
