ce839bba7d11ee215aea3f957dca35bec5b1a042
sylwek3100 <sylwek3100@gmail.com>
2014-12-19 12:39:26 +0100

    Fix view notification if workout is pause or stop

--- src/com/example/freetrackgps/GPSListner.java
+++ src/com/example/freetrackgps/GPSListner.java
@@ -35,8 +35,10 @@ public class GPSListner implements LocationListener  {
         	localRoute.addPoint(location);
         	this.workoutDistance.setText(String.format("%.2f", localRoute.getDistance()) + " m");
             this.workoutSpeed.setText(String.format("%d", (int)location.getSpeed() ) +" km/h");
-            notify.setContent(currentContext.getString(R.string.workoutDistanceLabel)+": " +  String.format("%.2f", localRoute.getDistance())+" m");
-            notify.sendNotyfi();
+            if(localRoute.getStatus() == RouteManager.routeStatus.start) {
+                notify.setContent(currentContext.getString(R.string.workoutDistanceLabel) + ": " + String.format("%.2f", localRoute.getDistance()) + " m");
+                notify.sendNotyfi();
+            }
         }
         this.gpsPosition.setText(message);
     }
