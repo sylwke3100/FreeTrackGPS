3d8d6a6a8313191e1662b656a2d2beb09fd63365
sylwek3100 <sylwek3100@gmail.com>
2015-01-29 00:38:06 +0100

    Separate operation for workout from activity to other class

--- src/com/example/freetrackgps/WorkoutsPreviewActivity.java
+++ src/com/example/freetrackgps/WorkoutsPreviewActivity.java
@@ -1,9 +1,7 @@
 package com.example.freetrackgps;
 
 import android.app.Activity;
-import android.content.ContextWrapper;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,40 +9,25 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.Toast;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 
 public class WorkoutsPreviewActivity extends Activity {
     private SimpleAdapter simpleAdapter;
-    private static final SimpleDateFormat formatDate = new SimpleDateFormat("dd.MM.yyyy HH:mm ");
-    private DatabaseManager currentDataBase;
+
+    private WorkoutsPreviewOperations workoutsPreviewOperations;
     private ListView listWorkout;
-    private List<RouteListElement> elements;
-    private SimpleDateFormat fileGpxFormat = new SimpleDateFormat("yyyy-MM-dd-HH_mm_ss");
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.workout_views_activity);
         listWorkout = (ListView) this.findViewById(R.id.listWorkout);
-        currentDataBase = new DatabaseManager(getBaseContext());
         registerForContextMenu(listWorkout);
+        workoutsPreviewOperations = new WorkoutsPreviewOperations(getBaseContext());
         onUpdateWorkoutsList();
     }
-    private void onUpdateWorkoutsList(){
-        elements = currentDataBase.getRoutesList();
-        ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
-        for(RouteListElement element: elements){
-            HashMap<String,String> simpleItem = new HashMap<String, String>();
-            simpleItem.put("time", formatDate.format(element.startTime));
-            simpleItem.put("distance",  String.format("%.2f km", element.distance/1000));
-            list.add(simpleItem);
-        }
-        simpleAdapter = new SimpleAdapter(this, list, R.layout.lines_layout, new String[]{"time", "distance"},new int[] {R.id.line_time, R.id.line_distance});
+
+    private void onUpdateWorkoutsList() {
+        simpleAdapter = new SimpleAdapter(this, workoutsPreviewOperations.getUpdatedWorkoutsList(), R.layout.lines_layout, new String[]{"time", "distance"}, new int[]{R.id.line_time, R.id.line_distance});
         listWorkout.setAdapter(simpleAdapter);
     }
 
@@ -59,35 +42,14 @@ public class WorkoutsPreviewActivity extends Activity {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
         switch (item.getItemId()) {
             case R.id.action_workout_delete:
-                currentDataBase.deleteRoute(elements.get(info.position).id);
+                workoutsPreviewOperations.deleteWorkout(info.position);
                 this.onUpdateWorkoutsList();
                 return true;
             case R.id.action_workout_export:
-                onExportWorkout(elements.get(info.position));
+                workoutsPreviewOperations.exportWorkout(info.position);
                 return true;
             default:
                 return super.onContextItemSelected(item);
         }
-    }
-
-    public void onExportWorkout(RouteListElement object){
-        List<RouteElement> pointsWorkout = currentDataBase.getPointsInRoute(object.id);
-        GPXWriter gpx;
-        StringBuffer fileNameBuffer = new StringBuffer();
-        ContextWrapper c = new ContextWrapper(getBaseContext());
-        File dir = new File(Environment.getExternalStorageDirectory()+"/workout/");
-        if(!(dir.exists() && dir.isDirectory()))
-            dir.mkdir();
-        fileNameBuffer.append(Environment.getExternalStorageDirectory() + "/workout/");
-        fileNameBuffer.append(fileGpxFormat.format(new Date(object.startTime)) + ".gpx");
-        gpx = new GPXWriter(fileNameBuffer.toString(), object.startTime);
-        for (RouteElement point: pointsWorkout){
-            gpx.addPoint(point);
-        }
-        if(gpx.save() == true)
-            Toast.makeText(getBaseContext(), getBaseContext().getString(R.string.SaveTrueInfo) + " " + fileNameBuffer.toString(), Toast.LENGTH_LONG).show();
-        else
-            Toast.makeText(getBaseContext(), getBaseContext().getString(R.string.SaveFalseInfo), Toast.LENGTH_LONG).show();
-
     }
 }
---
+++ src/com/example/freetrackgps/WorkoutsPreviewOperations.java
@@ -0,0 +1,64 @@
+package com.example.freetrackgps;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Environment;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+public class WorkoutsPreviewOperations {
+    private DatabaseManager currentDataBase;
+    private List<RouteListElement> elements;
+    private Context localContext;
+    private static final SimpleDateFormat formatDate = new SimpleDateFormat("dd.MM.yyyy HH:mm ");
+    private SimpleDateFormat fileGpxFormat = new SimpleDateFormat("yyyy-MM-dd-HH_mm_ss");
+    WorkoutsPreviewOperations(Context context){
+        currentDataBase = new DatabaseManager(context);
+        localContext = context;
+    }
+
+    public ArrayList<HashMap<String,String>> getUpdatedWorkoutsList(){
+        elements = currentDataBase.getRoutesList();
+        ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
+        for(RouteListElement element: elements){
+            HashMap<String,String> simpleItem = new HashMap<String, String>();
+            simpleItem.put("time", formatDate.format(element.startTime));
+            simpleItem.put("distance",  String.format("%.2f km", element.distance/1000));
+            list.add(simpleItem);
+        }
+        return list;
+    }
+
+    public void deleteWorkout(int id){
+        currentDataBase.deleteRoute(elements.get(id).id);
+    }
+
+    public void exportWorkout(int id){
+        RouteListElement object= elements.get(id);
+        List<RouteElement> pointsWorkout = currentDataBase.getPointsInRoute(object.id);
+        GPXWriter gpx;
+        StringBuffer fileNameBuffer = new StringBuffer();
+        ContextWrapper c = new ContextWrapper(localContext);
+        File dir = new File(Environment.getExternalStorageDirectory()+"/workout/");
+        if(!(dir.exists() && dir.isDirectory()))
+            dir.mkdir();
+        fileNameBuffer.append(Environment.getExternalStorageDirectory() + "/workout/");
+        fileNameBuffer.append(fileGpxFormat.format(new Date(object.startTime)) + ".gpx");
+        gpx = new GPXWriter(fileNameBuffer.toString(), object.startTime);
+        for (RouteElement point: pointsWorkout){
+            gpx.addPoint(point);
+        }
+        if(gpx.save() == true)
+            Toast.makeText(localContext, localContext.getString(R.string.SaveTrueInfo) + " " + fileNameBuffer.toString(), Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(localContext, localContext.getString(R.string.SaveFalseInfo), Toast.LENGTH_LONG).show();
+
+    }
+}
