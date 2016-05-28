d88a6a2f224e73f4231b8eaaa1ee628d7d55d7c2
sylwek3100 <sylwek3100@gmail.com>
2016-05-28 15:40:45 +0200

    Add circle with limit distance ignorePoint

--- src/com/sylwke3100/freetrackgps/IgnorePointsAddFromMapActivity.java
+++ src/com/sylwke3100/freetrackgps/IgnorePointsAddFromMapActivity.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 import org.osmdroid.bonuspack.overlays.MapEventsOverlay;
 import org.osmdroid.bonuspack.overlays.MapEventsReceiver;
+import org.osmdroid.bonuspack.overlays.Polygon;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
@@ -40,20 +41,23 @@ public class IgnorePointsAddFromMapActivity extends Activity {
         mMapView.setTilesScaledToDpi(true);
         mMapView.getController().setZoom(1);
         MapEventsOverlay eventsOverlay =
-            new MapEventsOverlay(mMapView.getContext(), new MapEventsReceiver() {
-                public boolean singleTapConfirmedHelper(GeoPoint geoPoint) {
-                    if (mMapView.getOverlays().size() > 1)
-                        mMapView.getOverlays().remove(1);
-                    mMapView.getOverlays()
-                        .add(createOverlayFromGeoPoint(mMapView.getContext(), geoPoint));
-                    mMapView.invalidate();
-                    return false;
-                }
+                new MapEventsOverlay(mMapView.getContext(), new MapEventsReceiver() {
+                    public boolean singleTapConfirmedHelper(GeoPoint geoPoint) {
+                        if (mMapView.getOverlays().size() > 2)
+                            mMapView.getOverlays().remove(2);
+                        if (mMapView.getOverlays().size() > 1)
+                            mMapView.getOverlays().remove(1);
+                        mMapView.getOverlays().add(addCircleWithLimitDistance(geoPoint));
+                        mMapView.getOverlays()
+                                .add(createOverlayFromGeoPoint(mMapView.getContext(), geoPoint));
+                        mMapView.invalidate();
+                        return false;
+                    }
 
-                public boolean longPressHelper(GeoPoint geoPoint) {
-                    return false;
-                }
-            });
+                    public boolean longPressHelper(GeoPoint geoPoint) {
+                        return false;
+                    }
+                });
         mMapView.getOverlays().add(0, eventsOverlay);
         ignorePointsManager = new IgnorePointsManager(getApplicationContext());
     }
@@ -67,9 +71,9 @@ public class IgnorePointsAddFromMapActivity extends Activity {
         switch (item.getItemId()) {
             case R.id.action_ignorepoints_add_centermap:
                 LocationSharing.LocationSharingResult location =
-                    sharedLocation.getCurrentLocation();
+                        sharedLocation.getCurrentLocation();
                 mMapView.getController()
-                    .setCenter(new GeoPoint(location.latitude, location.longitude));
+                        .setCenter(new GeoPoint(location.latitude, location.longitude));
                 mMapView.invalidate();
                 break;
             case R.id.action_ignorepoints_add_done:
@@ -82,7 +86,7 @@ public class IgnorePointsAddFromMapActivity extends Activity {
     }
 
     public boolean onPrepareOptionsMenu(Menu menu) {
-        if(ignorePoint == null)
+        if (ignorePoint == null)
             menu.findItem(R.id.action_ignorepoints_add_done).setEnabled(false);
         else
             menu.findItem(R.id.action_ignorepoints_add_done).setEnabled(true);
@@ -93,49 +97,61 @@ public class IgnorePointsAddFromMapActivity extends Activity {
         return super.onPrepareOptionsMenu(menu);
     }
 
+    private Polygon addCircleWithLimitDistance(GeoPoint point) {
+        Polygon polygon = new Polygon(this);
+        final double distance = 100;
+        ArrayList<GeoPoint> circlePoints = new ArrayList<GeoPoint>();
+        for (float degrees = 0; degrees < 360; degrees += 1) {
+            circlePoints.add(new GeoPoint(point.getLatitude(), point.getLongitude()).destinationPoint(distance, degrees));
+        }
+        polygon.setPoints(circlePoints);
+        return polygon;
+    }
+
 
     private ItemizedIconOverlay<OverlayItem> createOverlayFromGeoPoint(Context mapContext,
-        GeoPoint point) {
+                                                                       GeoPoint point) {
         OverlayItem overlayItem =
-            new OverlayItem("Ignored Point", "This point has been ignored", point);
+                new OverlayItem("Ignored Point", "This point has been ignored", point);
         ignorePoint = point;
         final List<OverlayItem> items = new ArrayList<OverlayItem>();
         items.add(overlayItem);
         return new ItemizedIconOverlay<OverlayItem>(mapContext, items,
-            new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
-                public boolean onItemSingleTapUp(int i, OverlayItem overlayItem) {
-                    return false;
-                }
+                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+                    public boolean onItemSingleTapUp(int i, OverlayItem overlayItem) {
+                        return false;
+                    }
 
-                public boolean onItemLongPress(int i, OverlayItem overlayItem) {
-                    return false;
-                }
-            });
+                    public boolean onItemLongPress(int i, OverlayItem overlayItem) {
+                        return false;
+                    }
+                });
     }
 
     public void onAddIgnorePointsFromLocation(String ignorePointName) {
         if (ignorePoint != null) {
             if (!ignorePointsManager
-                .addIgnorePoint(ignorePoint.getLatitude(), ignorePoint.getLongitude(),
-                    ignorePointName))
+                    .addIgnorePoint(ignorePoint.getLatitude(), ignorePoint.getLongitude(),
+                            ignorePointName))
                 Toast.makeText(getBaseContext(), R.string.ignorePointsExists, Toast.LENGTH_LONG)
-                    .show();
+                        .show();
         }
     }
 
     public void onSetIgnorePointsNameAlert() {
         LayoutInflater layoutInflater = LayoutInflater.from(this);
-        View promptView = layoutInflater.inflate(R.layout.prompt_ignore_points_from_location, null);
+        View promptView = layoutInflater.inflate
+                (R.layout.prompt_ignore_points_from_location, null);
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setView(promptView);
         final EditText inputName = (EditText) promptView.findViewById(R.id.nameEdit);
         alertDialogBuilder.setCancelable(false)
-            .setPositiveButton(R.string.okLabel, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    onAddIgnorePointsFromLocation(inputName.getText().toString());
-                    finish();
-                }
-            }).setNegativeButton(R.string.cancelLabel, new DialogInterface.OnClickListener() {
+                .setPositiveButton(R.string.okLabel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        onAddIgnorePointsFromLocation(inputName.getText().toString());
+                        finish();
+                    }
+                }).setNegativeButton(R.string.cancelLabel, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 dialog.cancel();
                 finish();
